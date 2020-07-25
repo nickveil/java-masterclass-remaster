ba3d20b8585c604eaeab496593cfb2d33ee053f8
nickveil <31191433+nickveil@users.noreply.github.com>
2020-07-25 10:42:20 -0500

    Classes Sum Calculator exercise solution

---
+++ Classes/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car ford = new Car();
+        porsche.setModel("Carrera");
+        System.out.println("Model is " + porsche.getModel());
+    }
+}
