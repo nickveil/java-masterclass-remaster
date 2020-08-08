52fca1c2fe82e503e8b5c4233a58e9454de34507
nickveil <31191433+nickveil@users.noreply.github.com>
2020-08-08 06:30:43 -0500

    Arrays Challenge solution with update

--- ArrayChallenge/src/com/company/Main.java
+++ ArrayChallenge/src/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
     private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        int[] givenIntegers = getIntegers(5);
+        int[] givenIntegers = getIntegers(3);
         printIntegers(sortIntegers(givenIntegers));
     }
 
@@ -21,22 +22,42 @@ public class Main {
     }
 
     public static int [] sortIntegers(int[] array){
+        int[] sortedArray = new int[array.length];
+        for (int i = 0; i<array.length; i++){
+            sortedArray[i] = array[i];
+        }
+//        int[] sortedArray = Arrays.copyOf(array,array.length)
+        boolean flag = true;
         int temp;
-        for (int i =0;i<array.length; i++){
-           for (int j=i+1; j<array.length; j++){
-               if (array[i]< array[j]){
-                   temp = array[i];
-                   array[i]=array[j];
-                   array[j] = temp;
-               }
+
+        while (flag){
+            flag=false;
+
+            for (int i = 0; i<sortedArray.length-1; i++){
+                if (sortedArray[i] < sortedArray[i+1]){
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
             }
         }
-        return array;
+        return sortedArray;
+//        for (int i =0;i<array.length; i++){
+//           for (int j=i+1; j<array.length; j++){
+//               if (array[i]< array[j]){
+//                   temp = array[i];
+//                   array[i]=array[j];
+//                   array[j] = temp;
+//               }
+//            }
+//        }
+//        return array;
     }
 
     public static void printIntegers(int[] sortedArray){
         for (int i=0;i<sortedArray.length; i++){
-            System.out.println(sortedArray[i]);
+            System.out.println("Element "+ i + " contents " + sortedArray[i]);
         }
     }
 }
