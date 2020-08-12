78192720eff18745d49a917525821f9f079b6dfa
nickveil <31191433+nickveil@users.noreply.github.com>
2020-08-12 12:38:54 -0500

    Banking App Challenge - added methods to bank class and tested final

--- SimpleBankingApplication/src/com/company/Bank.java
+++ SimpleBankingApplication/src/com/company/Bank.java
@@ -4,22 +4,69 @@ import java.util.ArrayList;
 
 public class Bank {
     private String bankName;
-    private ArrayList<Branch> branches = new ArrayList<Branch>();
+    private ArrayList<Branch> branches ;
 
-    public Bank(String bankName, ArrayList<Branch> branches) {
+    public Bank(String bankName) {
         this.bankName = bankName;
-        this.branches = branches;
+        this.branches = new ArrayList<Branch>();
+    }
+    public boolean addBranch (String branchName){
+        if (findBranch(branchName) == null){
+            this.branches.add(new Branch(branchName));
+            return true;
+        }
+        return false;
     }
 
-    public String getBankName() {
-        return bankName;
+    public boolean addCustomer (String branchName, String customerName, double initialAmount){
+        Branch branch = findBranch(branchName);
+        if (branch != null){
+            return branch.newCustomer(customerName,initialAmount);
+        }
+        return false;
     }
 
-    public ArrayList<Branch> getBranches() {
-        return branches;
+    public boolean addCustomerTransaction (String branchName, String customerName, double amount){
+        Branch branch = findBranch(branchName);
+        if (branch != null){
+            return branch.addCustomerTransaction(customerName, amount);
+        }
+        return false;
     }
 
-    public void addBranch (Branch branchName){
-        branches.add(branchName);
+    private Branch findBranch(String branchName){
+        for (int i=0; i<branches.size(); i++){
+            Branch checkedBranches = this.branches.get(i);
+            if (checkedBranches.getBranchName().equals(branchName)){
+                return checkedBranches;
+            }
+        }
+        return null;
     }
+
+    public boolean listCustomers (String branchName, boolean showTransactions){
+        Branch branch = findBranch(branchName);
+        if (branch!=null){
+            System.out.println("Customers details for branch " + branch.getBranchName());
+
+            ArrayList<Customer> branchCustomers = branch.getCustomers();
+            for (int i=0; i<branchCustomers.size(); i++){
+                Customer branchCustomer = branchCustomers.get(i);
+                System.out.println("Customer: " + branchCustomer.getCustomerName() +"[" + (i+1) +"]");
+                if (showTransactions){
+                    System.out.println("Transactions:");
+                    ArrayList<Double> transactions = branchCustomer.getTransactions();
+                    for (int j=0; j<transactions.size(); j++){
+                        System.out.println("Transaction " + (j+1) + ": amount: $" + transactions.get(j) );
+                    }
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
 }
--- SimpleBankingApplication/src/com/company/Main.java
+++ SimpleBankingApplication/src/com/company/Main.java
@@ -3,6 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Bank bank = new Bank("Union Bank");
+
+	    bank.addBranch("Old Chenny");
+	    bank.addCustomer("Old Chenny", "Mike", 50.03);
+        bank.addCustomer("Old Chenny", "Carter", 78.43);
+        bank.addCustomer("Old Chenny", "Hannah", 1000.67);
+        bank.addCustomerTransaction("Old Chenny", "Hannah", 25.44);
+        bank.addCustomerTransaction("Old Chenny", "Mike", 54.44);
+        bank.addCustomerTransaction("Old Chenny", "Carter", 25.76);
+        bank.addCustomerTransaction("Old Chenny", "Hannah", 33.44);
+        bank.addCustomerTransaction("Old Chenny", "Hannah", 25.55);
+
+
+        bank.addBranch("Stacy Lane");
+        bank.addCustomer("Stacy Lane", "John",54.56);
+        bank.addCustomerTransaction("Stacy Lane", "John", 25.43);
+
+
+        bank.addCustomerTransaction("Old Chenny","John", 45.33);
+
+        bank.listCustomers("Old Chenny",true);
+        bank.listCustomers("Stacy Lane",false);
+        
     }
 }
